31d425b3974b0d17b7dfedf6a469fe685d79113f
xiaoshi <sjl_leaf@163.com>
2015-12-09 10:50:53 +0800

    Initial Commit

---
+++ Navigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Navigation
+//
+//  Created by 史金亮 on 15/12/9.
+//  Copyright © 2015年 kamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
